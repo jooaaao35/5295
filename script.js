a0d3aff1767fbdf4d36199d17b7f90659d9ae325
jooaaao35 <joaof0666@gmail.com>
2025-06-26 20:17:25 -0300

    1

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 alert("comando para mostrar um texto na tela");
 alert("ele é limitado ao cique no ok pelo usuário");
 alert("não é possivel configurar sem tempo de permanencia na tela");
+let numero = 25;
+let chute;
+let idade;
+let nome;
+
+console.log (numero);
