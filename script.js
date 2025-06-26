8d4e5b8cbabbc5d752a464f9fcda4427425a19ca
jooaaao35 <joaof0666@gmail.com>
2025-06-26 20:36:47 -0300

    9

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ let chute;
 let idade;
 let nome;
 
-nome = console.log(pronpt("digite seu nome:"));
+nome = console.log(prompt("digite seu nome:"));
+
+
+let entrada = prompt("Digite um número inteiro:");
+let numero = parselnt(entrada);
+
+if(Number.islnteger(numero)){
+    console.log("Obrigado! Você digitou um inteiro.");
+} else {
+    console.log("Por favor, digite um número inteiro válido");
+}
